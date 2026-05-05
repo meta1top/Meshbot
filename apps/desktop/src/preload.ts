923185679002d343d5476f196ee6875bf4ba3fe0
Grant <284885069@qq.com>
2026-05-05 22:08:22 +0800

    feat: 控制按钮

--- apps/desktop/src/preload.ts
+++ apps/desktop/src/preload.ts
@@ -1,6 +1,9 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("electronAPI", {
   isElectron: true,
   platform: process.platform,
+  windowMinimize: () => ipcRenderer.invoke("window-minimize"),
+  windowMaximize: () => ipcRenderer.invoke("window-maximize"),
+  windowClose: () => ipcRenderer.invoke("window-close"),
 });
