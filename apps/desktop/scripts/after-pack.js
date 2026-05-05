45e037d9dc899c7f50979c0d904db14581ca6f28
Grant <284885069@qq.com>
2026-05-05 21:20:56 +0800

    fix: linux

--- apps/desktop/scripts/after-pack.js
+++ apps/desktop/scripts/after-pack.js
@@ -8,4 +8,15 @@ exports.default = async function afterPack(context) {
   if (fs.existsSync(sandboxPath)) {
     fs.unlinkSync(sandboxPath);
   }
+
+  const execName = context.packager.appInfo.productFilename;
+  const execPath = path.join(context.appOutDir, execName);
+  const realExecPath = path.join(context.appOutDir, `${execName}.bin`);
+
+  fs.renameSync(execPath, realExecPath);
+  fs.writeFileSync(
+    execPath,
+    `#!/bin/bash\nexec "$(dirname "$0")/${execName}.bin" --no-sandbox "$@"\n`,
+    { mode: 0o755 },
+  );
 };
